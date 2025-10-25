209c0299c6b7459a42d7f42e74ffb2c89edd275c
yangelia <yana.sychova@gmail.com>
2025-10-26 02:40:13 +0300

    edits noteService

--- src/services/noteService.ts
+++ src/services/noteService.ts
@@ -1,5 +1,13 @@
-import { api } from "../api/axiosInstance";
+import axios from "axios";
 import type { Note, CreateNoteRequest } from "../types/note";
+
+const API_URL = "https://notehub-public.goit.study/api/notes";
+const token = import.meta.env.VITE_NOTEHUB_TOKEN;
+
+const headers = {
+  Authorization: `Bearer ${token}`,
+  "Content-Type": "application/json",
+};
 
 export interface FetchNotesResponse {
   notes: Note[];
@@ -11,25 +19,29 @@ export const fetchNotes = async (
   perPage: number = 12,
   query?: string
 ): Promise<FetchNotesResponse> => {
-  const params: Record<string, string | number> = {
-    page,
-    perPage,
-  };
+  const params = new URLSearchParams({
+    page: page.toString(),
+    perPage: perPage.toString(),
+    sortBy: "createdAt",
+  });
 
   if (query && query.trim()) {
-    params.search = query;
+    params.append("search", query);
   }
 
-  const { data } = await api.get<FetchNotesResponse>("/notes", { params });
-  return data;
+  const response = await axios.get<FetchNotesResponse>(
+    `${API_URL}?${params.toString()}`,
+    { headers }
+  );
+  return response.data;
 };
 
 export const createNote = async (newNote: CreateNoteRequest): Promise<Note> => {
-  const { data } = await api.post<Note>("/notes", newNote);
-  return data;
+  const response = await axios.post<Note>(API_URL, newNote, { headers });
+  return response.data;
 };
 
 export const deleteNote = async (id: string): Promise<Note> => {
-  const { data } = await api.delete<Note>(`/notes/${id}`);
-  return data;
+  const response = await axios.delete<Note>(`${API_URL}/${id}`, { headers });
+  return response.data;
 };
